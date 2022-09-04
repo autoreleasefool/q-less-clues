3cb50bcdf54b3bca8a8a95c52b142376a89825d0
Joseph Roque <contact@josephroque.ca>
2022-09-04 09:30:50 -0700

    Initial project commit

---
+++ QLessClues/QLessClues/Source/QLessCluesApp.swift
@@ -0,0 +1,17 @@
+//
+//  QLessCluesApp.swift
+//  QLessClues
+//
+//  Created by Joseph Roque on 2022-09-04.
+//
+
+import SwiftUI
+
+@main
+struct QLessCluesApp: App {
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
+}
